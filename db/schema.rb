30f142968d7c95bafae1297758914bfe2804b51c
sulo78 <jan@soundpauli.de>
2013-03-18 19:10:47 +0100

    installed gem bootsrap-sass

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130310162857) do
+ActiveRecord::Schema.define(:version => 20130318141324) do
 
   create_table "lendings", :force => true do |t|
     t.string   "lender_name"
