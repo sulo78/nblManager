9ec79d6b9f6d317ac6886453d82356044e5d4814
sulo78 <jan@soundpauli.de>
2013-03-24 17:17:20 +0100

    implemented lendings step-1

--- app/controllers/lendings_controller.rb
+++ app/controllers/lendings_controller.rb
@@ -1,7 +1,15 @@
 class LendingsController < ApplicationController
+
+# HTTP-GET-Methoden (views)
+
+# GET /lendings/index
   def index
+    @location = Location.all
+    @notebooks = Notebook.all
   end
 
+# GET /lendings/new
   def new
+    @lending = Lending.new
   end
 end
