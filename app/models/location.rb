68684218ce6dad6a6188bc20ff02b693e1c05c46
sulo78 <jan@soundpauli.de>
2013-04-22 12:45:52 +0200

    Milestone 1 - Alpha version ready :)

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,5 @@ class Location < ActiveRecord::Base
   has_many :notebooks
   attr_accessible :loc_acronym
   validates_presence_of :loc_acronym, :message => "sollte schon eingetragen werden! o.O"
+  validates_uniqueness_of :loc_acronym
 end
--- app/models/pool.rb
+++ app/models/pool.rb
@@ -2,4 +2,5 @@ class Pool < ActiveRecord::Base
   has_many :notebooks
   attr_accessible :pool_name
   validates_presence_of :pool_name, :message => "sollte schon eingetragen werden! o.O "
+  validates_uniqueness_of :pool_name
 end
